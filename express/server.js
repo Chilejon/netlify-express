27df53bd10631d2b15adb374e9d88b694e33cb63
jon.chiles <jonchiles@yahoo.com>
2019-07-04 19:48:03 +0100

    testing

--- express/server.js
+++ express/server.js
@@ -1,20 +1,54 @@
-'use strict';
-const express = require('express');
-const serverless = require('serverless-http');
-const app = express();
-const bodyParser = require('body-parser');
+// 'use strict';
+// const express = require('express');
+// const serverless = require('serverless-http');
+// const app = express();
+// const bodyParser = require('body-parser');
 
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+// const router = express.Router();
+// router.get('/', (req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/html' });
+//   res.write('<h1>Hello from Express.js!</h1>');
+//   res.end();
+// });
+// router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+// router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+// app.use(bodyParser.json());
+// app.use('/.netlify/functions/server', router);  // path must route to lambda
+
+// module.exports = app;
+// module.exports.handler = serverless(app);
+
+
+//new
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+   res.sendfile('index.html');
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+users = [];
+io.on('connection', function(socket) {
+   console.log('A user connected');
+   socket.on('setUsername', function(data) {
+      console.log(data);
+      
+      if(users.indexOf(data) > -1) {
+         socket.emit('userExists', data + ' username is taken! Try some other username.');
+      } else {
+         users.push(data);
+         socket.emit('userSet', {username: data});
+      }
+   });
+   
+   socket.on('msg', function(data) {
+      //Send message to everyone
+      io.sockets.emit('newmsg', data);
+   })
+});
 
-module.exports = app;
-module.exports.handler = serverless(app);
+http.listen(3000, function() {
+   console.log('listening on localhost:3000');
+});
